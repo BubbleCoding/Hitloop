f6acc96f9f711fe6c58e460eb9b10c7dba8f9c17
Bas Pijls-van Kooten <b.pijls@hva.nl>
2025-05-30 18:05:34 +0200

    Add binary files for accelerometer strand test including main firmware, bootloader, partitions, and upload script for ESP32C3.

---
+++ firmware/hardware_test/build/esp32.esp32.XIAO_ESP32C3/upload.bat
@@ -0,0 +1,3 @@
+rem Usage: upload.bat COM Port
+rem Replace the location of the ESPTool.exe for your specific configuration
+C:\Users\bpijls\AppData\Local\Arduino15\packages\esp32\tools\esptool_py\4.9.dev3\esptool.exe --chip esp32c3 --port %1 --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode keep --flash_freq keep --flash_size keep 0x0 "accelero_strandtest.ino.bootloader.bin" 0x8000 "accelero_strandtest.ino.partitions.bin" 0xe000 "boot_app0.bin" 0x10000 "accelero_strandtest.ino.bin"
