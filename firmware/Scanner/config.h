1a9f6d221f05c1247c6c58041c3dafb44fc1e4ed
Bas Pijls-van Kooten <b.pijls@hva.nl>
2025-06-05 22:32:05 +0200

    Add configuration header for Scanner firmware: define constants for scanner and WiFi settings, including scan intervals and button pin.

---
+++ firmware/Scanner/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SCANNER_NAME "Scanner"
+#define BEACON_NAME_PREFIX "HitloopBeacon"
+#define BLE_SCAN_INTERVAL 100
+#define BLE_SCAN_WINDOW 99
+#define WIFI_CONNECT_DELAY 500
+#define WIFI_SEND_DELAY 3000
+#define SERIAL_BAUD_RATE 115200
+#define SETUP_DELAY 1000
+#define SCAN_TIME 10
+
+#define BOOT_BUTTON_PIN 9
+
+#endif
+
